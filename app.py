112313a2fd33ca5abfaa3de41fcbfd25b2524f28
sheikhadil12 <sheikhadil00015@gmail.com>
2026-01-16 11:00:23 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "API RUNNING OK"
 
 @app.route("/join")
 def join():
-    return {"status":"ok"}
+    tc = request.args.get("tc")
+    uid = request.args.get("uid1")
+    emote = request.args.get("emote_id")
+
+    if not tc or not uid or not emote:
+        return jsonify({"error": "missing params"}), 400
+
+    return jsonify({
+        "status": "success",
+        "tc": tc,
+        "uid": uid,
+        "emote": emote
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
